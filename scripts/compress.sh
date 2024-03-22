6c00498db6e25200f98f0a657a6aa925293819a3
sachinisiriwardene <sachinisiriwardene@gmail.com>
2024-03-22 12:13:20 +0530

    update compress script

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -1,1 +1,1 @@
-cd public/ && zip -r $1.zip ./* && cd - && zip -d public/$1 resources/APIMetadata/*
+rm -f $1.zip && cd public/ && zip -r $1.zip ./* && cd - && zip -d public/$1 resources/content-mock/*
